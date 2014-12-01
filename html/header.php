a4e6b385ea57288ca1d820b1b28a01b038fda136
dionkoers <dionkoers@gmail.com>
2014-12-01 13:45:54 +0100

    Css logo.png aangemaakt en vervangen met logo2 Menu Login

---
+++ html/header.php
@@ -0,0 +1,11 @@
+        <div id="topBalk">
+            <div class="four columns">
+                <img src="images/logo2.png">
+            </div>
+            <div class="ten columns">
+                <img src="images/headerImageTest.jpg">
+            </div>  
+            <div class="two columns">
+                <p>HIER KOMT INLOG GEDEELTE -- INLOG INLOG</p>
+            </div> 
+	   </div>
