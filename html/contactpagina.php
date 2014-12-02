c7371d30b5bf88a4acd2ba03d3c0c139b2c90981
shahin419 <shahen_thebeste@hotmail.com>
2014-12-02 11:53:15 +0100

    gefix

---
+++ html/contactpagina.php
@@ -0,0 +1,30 @@
+<!-- Contact pagina
+==================================================== -->
+<form name="test" method="post" action="test.php">
+<table width="450px">
+</tr>
+<tr>
+ <td valign="top">
+  <label for="Voornaam">Voornaam*</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="Voornaam" maxlength="50" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="Achternaam">Achternaam*</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="Achternaam" maxlength="50" size="30">
+ </td>
+</tr>
+
+<tr>
+ <td valign="top">
+  <label for="emaildres">E-mailadres*</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="emailadres" maxlength="80" size="30">
+ </td>
