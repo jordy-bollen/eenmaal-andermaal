d4b528ac86c205937b3bb48bd416c4ff128c1d02
shahin419 <shahen_thebeste@hotmail.com>
2014-12-03 15:42:38 +0100

    mijn account added

--- html/mijnaccount.php
+++ html/mijnaccount.php
@@ -70,41 +70,24 @@
                     <a href="#"> <li>rubiek 1</li></a>
                     <a href="#">  <li>rubiek 1</li></a>
                     <a href="#">  <li>rubiek 1</li></a>
-                    <a href="#">  <li>rubiek 1</li></a>
-                    <a href="#">  <li>rubiek 1</li></a>
-                    <a href="#">  <li>rubiek 1</li></a>
-                    <a href="#">  <li>rubiek 1</li></a>
-                    <a href="#">  <li>rubiek 1</li></a>
-                    <a href="#">  <li>rubiek 1</li></a>
-                    <a href="#">  <li>rubiek 1</li></a>
                 </ul>
             </div>
         
         <!-- Contact
         ==================================================== -->
         <div id="content">
-            <div class="four column tekstlables">
-                <p class="pmargin">Aanhef*</p>
-                <p class="pmargin">Voornaam*</p>
-                <p class="pmargin">Achternaam*</p>
-                <p class="pmargin">E-mailadres*</p>
-                <p class="pmargin">Telefeoon</p>
-                <p class="pmargin">Onderwerp*</p>
-                <p class="pmargin">Bericht*</p>
+            <div class="twelve columns main">
+                <div class="six columns">
+                    <p class="test">Mijn Biedingen: <a href="http://localhost"><button>Go</button></p></a>
+                    <p class="test">Mijn Advertenties: <a href="http://localhost"><button>Go</button></p></a>
+                    <p class="test">Mijn Verkopers Account Registreren: <a href="http://localhost"><button>Go</button></p></a>
+                </div>
+            <div class="six columns">
+                    <p class="test1">Wijzigen Persoonsgegvens: <a href="http://localhost"><button>Go</button></p></a>
+                    <p class="test1">Wijzigen Bankgegevens: <a href="http://localhost"><button>Go</button></p></a>
+                    <p class="test1">Wijzigen Wachtwoord en Geheime Vraag: <a href="http://localhost"><button>Go</button></p></a>
             </div>
-
-            <div class="four column inputvelden">
-                <select name="aanhef" class="input">
-                    <option value="Dhr.">Dhr.</option>
-                    <option value="Mevr.">Mevr.</option>
-                </select>
-                <input type="text" name="Voornaam" maxlength="50" size="30">
-                <input type="text" name="Achternaam" maxlength="50" size="30">
-                <input type="text" name="emailadres" maxlength="80" size="30">
-                <input type="text" name="telefoon" maxlength="30" size="30">
-                <input type="text" name="onderwerp" maxlength="50" size="30">
-                <textarea  name="bericht" maxlength="1000" cols="40" rows="5"></textarea><p class="verplicht"><h6>Velden met * zijn verplicht.</p></h6>
-                <input type="submit" value="Versturen" class="verstuur">
+                
             </div>
 
            <!-- Footer 
