90c6cad2808c88ec144d4088aa50b780db6a169c
shahin419 <shahen_thebeste@hotmail.com>
2014-12-04 14:53:36 +0100

    wijzigenpersoonsgegevens added

--- html/registratiepagina.php
+++ html/registratiepagina.php
@@ -20,22 +20,22 @@
             <input type='text' name='voornaam' class="invoerveld" maxlength="255"/>
 
             <label for='achternaam' class="three columns">Achternaam*</label>
-            <input type='text' name='achternaam' class="invoerveld" maxlength="255" />
+            <input type='text' name='achternaam' class="invoerveld" maxlength="255"/>
 
             <label for='gebruikersnaam' class="three columns">Gebruikersnaam*</label>
-            <input type='text' name='gebruikersnaam' class="invoerveld" maxlength="50" />
+            <input type='text' name='gebruikersnaam' class="invoerveld" maxlength="50"/>
 
             <label for='email' class="three columns">E-mailadres*</label>
-            <input type='text' name='email' class="invoerveld" maxlength="50" />
+            <input type='text' name='email' class="invoerveld" maxlength="50"/>
 
             <label for='geboortedatum' class="three columns">Geboortedatum*</label>
-            <input type='date' name='geboortedatum' class="datum" maxlength="50" />
+            <input type='date' name='geboortedatum' class="datum" maxlength="50"/>
 
             <label for='wachtwoord' class="three columns">Wachtwoord*</label>
-            <input type='password' name='wachtwoord' class="invoerveld" maxlength="50" />
+            <input type='password' name='wachtwoord' class="invoerveld" maxlength="50"/>
 
             <label for='wachtwoord' class="three columns">Bevestig wachtwoord*</label>
-            <input type='password' name='wachtwoord' class="invoerveld" maxlength="50" />
+            <input type='password' name='wachtwoord' class="invoerveld" maxlength="50"/>
 
             <!-- Geheime vraag: nieuwe gebruiker moet een vraag kiezen
             ==================================================== -->    
@@ -46,25 +46,25 @@
             </select>
 
             <label for='Antwoord' class="three columns">Geheim antwoord*</label>
-            <input type='text' name='antwoord' class="invoerveld" maxlength="50" />
+            <input type='text' name='antwoord' class="invoerveld" maxlength="50"/>
         </div>
         <div class="six columns">
             <!-- Persoongegevens
             ==================================================== -->
             <label for='adres1' class="three columns">Adres één*</label>
-            <input type='text' name='adres1' class="invoerveld" maxlength="50" />
+            <input type='text' name='adres1' class="invoerveld" maxlength="50"/>
 
             <label for='adres2' class="three columns">Adres twee</label>
-            <input type='text' name='adres2' class="invoerveld" maxlength="50" />
+            <input type='text' name='adres2' class="invoerveld" maxlength="50"/>
 
             <label for='huisnummer' class="three columns">Huisnummer*</label>
-            <input type='text' name='huisnummer' class="invoerveld" maxlength="50" />
+            <input type='text' name='huisnummer' class="invoerveld" maxlength="50"/>
 
             <label for='postcode' class="three columns">Postcode*</label>
-            <input type='text' name='postcode' class="invoerveld" maxlength="50" />
+            <input type='text' name='postcode' class="invoerveld" maxlength="50"/>
 
             <label for='woonplaats' class="three columns">Woonplaats*</label>
-            <input type='text' name='plaats' class="invoerveld" maxlength="50" />
+            <input type='text' name='plaats' class="invoerveld" maxlength="50"/>
 
             <label for='land' class="three columns">Land*</label>
             <select name="land" class="lengte">
@@ -74,10 +74,10 @@
             </select>
 
             <label for='telefoonnummer' class="three columns">Telefoonnummer*</label>
-            <input type='text' name='telefoonnummer' class="invoerveld" maxlength="50" />
+            <input type='text' name='telefoonnummer' class="invoerveld" maxlength="50"/>
 
             <label for='mobiel' class="three columns">Mobiel*</label>
-            <input type='text' name='mobiel' class="invoerveld" maxlength=""50 />
+            <input type='text' name='mobiel' class="invoerveld" maxlength="50"/>
 
             <input type='submit' name='submit' value='Registreer' class="registratiebutton"/>
             <p><h6 class="veldenmet">Velden met een * zijn verplicht.</h6><p>
