d96a307959f47f45728beb2b8c96c7f4518daca3
Thomas van der Zee <thomasvdzee@gmail.com>
2014-12-11 15:06:33 +0100

    zoekresultaten aanpassingen

--- php/application/view/zoekresultaten.php
+++ php/application/view/zoekresultaten.php
@@ -1,17 +1,24 @@
- <?php while($resultaat = sqlsrv_fetch_object($data['zoekresultaten'])): ?>
+<?php
+$i = 0;
+while( $img = sqlsrv_fetch_object( $data['afbeeldingen'] )) {
+    $afbeeldingen[$i] = $img;
+    $i++;
+}
+$i = 0;
+while( $hoogsteboden = sqlsrv_fetch_object($data['hoogsteboden'])) {
+    $hoogstebod[$i] = $hoogsteboden;
+    $i++;
+}
+?> 
+
+
+<?php while($resultaat = sqlsrv_fetch_object($data['zoekresultaten'])): ?>
     
-
-
-
-
-
-
-
    <div class="four columns veiling">
-            <h3><a href="<?= SITE_URL ?>producten/<?= $obj->voorwerpnummer . '-' . trim( preg_replace( "/[^0-9a-z]+/i", "",str_replace(" ","-",strtolower($obj->titel)))) ?>"><?= $obj->titel ?></a></h3>
+            <h3><a href="<?= SITE_URL ?>producten/<?= $resultaat->voorwerpnummer . '-' . trim( preg_replace( "/[^0-9a-z]+/i", "",str_replace(" ","-",strtolower($resultaat->titel)))) ?>"><?= $resultaat->titel ?></a></h3>
             <?php for( $j = 0; $j < count($afbeeldingen); $j++ ):
 
-                 if($afbeeldingen[$j]->voorwerp == $obj->voorwerpnummer): ?>
+                 if($afbeeldingen[$j]->voorwerp == $resultaat->voorwerpnummer): ?>
 
             <img src="<?= SKINS_DIR ?>img/producten/<?php echo $afbeeldingen[$j]->filenaam; ?>">
                 <?php endif;
@@ -21,15 +28,16 @@
             </div>
             <script type="text/javascript">
                 $("#countdownTime<?= $i ?>")
-                    .countdown("<?php echo date_format($obj->eindmoment,'Y-m-d H:m:s'); ?>", function(event) {
+                    .countdown("<?php echo date_format($resultaat->eindmoment,'Y-m-d H:m:s'); ?>", function(event) {
                         $(this).text(
                             event.strftime('%D dagen %H:%M:%S')
                         );
                     });
             </script>
             <?php for( $j = 0; $j < count($hoogstebod); $j++ ):
-                if($hoogstebod[$j]->voorwerpnummer == $obj->voorwerpnummer): ?>
+                if($hoogstebod[$j]->voorwerpnummer == $resultaat->voorwerpnummer): ?>
             <p>Hoogste bod: <strong><?= $hoogstebod[$j]->hoogste ?></strong></p>
             <?php endif; endfor; ?>
         </div>
-            <?php endif; ?>
+        
+    <?php $i++; endwhile; ?>
