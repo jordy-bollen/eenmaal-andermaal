d5acda5c845a6ac999407ea86df3ba6dca4f02cf
Shahin419 <shahen_thebeste@hotmail.com>
2014-12-11 14:08:54 +0100

    feedback pagina gemaakt

---
+++ php/application/controller/feedback.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jordy
+ * Date: 5-12-14
+ * Time: 15:18
+ */
+
+class feedback extends controller{
+
+    function __constructor() {
+        parent::__construct();
+
+    }
+
+    public function index() {
+        $modelRubrieken = $this->loadModel('rubriek');
+        $rubrieken = $modelRubrieken->getHoofdRubrieken();
+        $this->data['rubrieken'] = $rubrieken;
+        $this->loadView('includes/header');
+        $this->loadView('feed-back', $this->data);
+        $this->loadView('includes/footer');
+    }
+} 
