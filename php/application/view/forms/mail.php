164db50acccd95a367a5f02054856ab66ec79e35
Thomas van der Zee <thomasvdzee@gmail.com>
2015-01-06 11:28:28 +0100

    no message

--- php/application/view/forms/mail.php
+++ php/application/view/forms/mail.php
@@ -1,6 +1,9 @@
 <form id='register' action='<?php echo SITE_URL ?>registreren' method='post'
       accept-charset='UTF-8'>
-<label for='email' >email*:</label>
+    <strong>Voer hier uw e-mailadres in zodat u deze kunt verifiëren.</strong>
+    <p></p>
+    <br>
+    <label for='email' >email*:</label>
         <input type='text' name='email' id='email' maxlength="50" /><br />
     <input type='submit' class="zoekknop" name='submitMail' value='Submit' />
 </form>
